09d1f2fc8ad99dd0d4506e046aacbdf33ed2055a
Adedolapo Adefaye <daintiestdolly@gmail.com>
2022-10-13 16:10:38 -0700

    C task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 /**
  *  * main-entry point for the program.
  *   *
- *    * Return:0 if no errors, return non zero if errors
- *     */
+ *    * Return: 0 if no errors, return non zero if errors
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
