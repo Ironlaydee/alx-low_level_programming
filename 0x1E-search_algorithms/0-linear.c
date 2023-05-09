e83033afef97c64052ebc9b18f9e740f010c27ef
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-05-10 00:17:27 +0100

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches a value in an array using a linear search.
+ * a function that searches for a value in an array of integers using the Linear search algorithm.
  * @array: The array to search in.
  * @size: The length of the array.
  * @value: The value to look for.
