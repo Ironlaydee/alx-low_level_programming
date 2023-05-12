becbb653a5d0ef6b7646cd524f7052beea1e9ca1
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-05-12 10:46:54 +0100

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,28 +1,43 @@
 #include "search_algos.h"
 
 /**
- * Linear_search - searches a value in an array using Linear search algorithms.
- * @array: The array to search in.
- * @size: The length of the array.
- * @value: The value to look for.
+ * binary_search - a function that searches for a value in a
+ * sorted array of integers using the Binary search algorithm.
+ * @array: pointer to first element of array to seach
+ * @size: number of elements in array
+ * @value: value to search for
  *
- * Return: The first index of the value in the array, otherwise -1.
+ * Return: return -1
  */
-int linear_search(int *array, size_t size, int value)
-{
-	size_t i;
 
-	for (i = 0; (i < size) && (array); i++)
+int binary_search(int *array, size_t size, int value)
+{
+	int low, mid, high;
+	int n;
+
+	if (array == NULL)
 	{
-		if (*(array + i) == value)
-		{
-			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
-			return (i);
-		}
-		else
-		{
-			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
-		}
+		return (-1);
 	}
+
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+
+		printf("Searching in array: ");
+		for (n = low; n <= high; n++)
+			printf("%i%s", array[n], n == high ? "\n" : ", ");
+
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			return (mid);
+	}
+
 	return (-1);
 }
